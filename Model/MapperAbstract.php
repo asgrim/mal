357e6e5a762eda6a9c47a904fc5c3aadfc37be91
James Titcumb <james@asgrim.com>
2011-07-30 23:35:24 +0100

    Save function now returns id on insert and rows affected on update

--- Model/MapperAbstract.php
+++ Model/MapperAbstract.php
@@ -1,13 +1,13 @@
 <?php
-	
+
 	/**
 	 * Provides a centralisation of methods that I found myself writing over
 	 * and over again.
-	 * 
+	 *
 	 * Makes mapping a Mapper class a bit easier - just implement a few
 	 * functions and it'll give you basic Mapper functionality automatically
 	 * so extend this because it's ace.
-	 * 
+	 *
 	 * Available "easy" functions are:
 	 *   - save($obj)
 	 *   - find($int, $obj)
@@ -22,35 +22,35 @@
 	abstract class MAL_Model_MapperAbstract
 	{
 		protected $_dbTable;
-		
+
 		/**
 		 * Should return the name of the table being used e.g. "Application_Model_DbTable_BlogPost"
 		 * @return string
 		 */
 		abstract protected function getDbTableName();
-		
+
 		/**
 		 * Should return the name of the object being used e.g. "Application_Model_BlogPost"
 		 * @return string
 		 */
 		abstract protected function getObjectName();
-		
+
 		/**
 		 * Should return an array of mapped fields to use in the MAL_Model_MapperAbstract::Save function
 		 * @param unknown_type $obj
 		 */
 		abstract protected function getSaveData($obj);
-		
+
 		/**
 		 * Implement this by setting $obj values (e.g. $obj->setId($row->Id) from a DB row
 		 * @param unknown_type $obj
 		 * @param Zend_Db_Table_Row_Abstract $row
 		 */
 		abstract protected function populateObjectFromRow(&$obj, Zend_Db_Table_Row_Abstract $row);
-		
+
 		/**
 		 * Create an instance of the database table
-		 * 
+		 *
 		 * @param string $dbTable You should generate this in MAL_Model_MapperAbstract::getDbTableName()
 		 * @throws Exception
 		 */
@@ -60,12 +60,12 @@
 			{
 				$dbTable = new $dbTable();
 			}
-			
+
 			if (!$dbTable instanceof Zend_Db_Table_Abstract)
 			{
 				throw new Exception('Invalid table data gateway provided');
 			}
-			
+
 			$this->_dbTable = $dbTable;
 			return $this;
 		}
@@ -82,25 +82,29 @@
 			}
 			return $this->_dbTable;
 		}
-		
+
 		/**
 		 * Save an object. Requires MAL_Model_MapperAbstract::getSaveData to be implemented!
+		 *
+		 * @return int On insert, returns pkey of new record. On update, returns num rows affected.
 		 */
 		public function save($obj)
 		{
 			$data = $this->getSaveData($obj);
-			
-			if(null === ($id = $obj->getId())) 
+
+			if(null === ($id = $obj->getId()))
 			{
 				unset($data['id']);
-				$this->getDbTable()->insert($data);
+				$new_id = $this->getDbTable()->insert($data);
+				$obj->setId($new_id);
+				return $new_id;
 			}
 			else
 			{
-				$this->getDbTable()->update($data, array('id = ?' => $id));
+				return $this->getDbTable()->update($data, array('id = ?' => $id));
 			}
 		}
-		
+
 		/**
 		 * Search for an object by it's ID
 		 * @param int $id
@@ -116,7 +120,7 @@
 			$row = $result->current();
 			$this->populateObjectFromRow($obj, $row);
 		}
-		
+
 		/**
 		 * Get the number of rows in this table being mapped
 		 */
@@ -126,12 +130,12 @@
 			$sql = $select->from($this->getDbTable(), 'COUNT(*)');
 			return $this->getDbTable()->getAdapter()->fetchOne($sql);
 		}
-		
+
 		/**
 		 * Fetch all the rows from a database (optionally with a where, order, limit, offset clause)
-		 * 
+		 *
 		 * Requires MAL_Model_MapperAbstract::getObjectName and MAL_Model_MapperAbstract::populateObjectFromRow to be implemented
-		 * 
+		 *
 		 * @param string|array|Zend_Db_Table_Select $where  OPTIONAL An SQL WHERE clause or Zend_Db_Table_Select object.
 		 * @param string|array                      $order  OPTIONAL An SQL ORDER clause.
 		 * @param int                               $count  OPTIONAL An SQL LIMIT count.
@@ -141,9 +145,9 @@
 		public function fetchAll($where = null, $order = null, $limit = null, $offset = null)
 		{
 			$resultSet = $this->getDbTable()->fetchAll($where, $order, $limit, $offset);
-			
+
 			$obj = $this->getObjectName();
-			
+
 			$obj_collection = array();
 			foreach ($resultSet as $row)
 			{
@@ -153,7 +157,7 @@
 			}
 			return $obj_collection;
 		}
-		
+
 		/**
 		 * Delete an object from the database table
 		 * @param unknown_type $obj
@@ -162,7 +166,7 @@
 		public function delete($obj)
 		{
 			$id = $obj->getId();
-			
+
 			if($id > 0)
 			{
 				return $this->getDbTable()->delete(array('id = ?' => $id));
